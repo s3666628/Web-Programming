20642abc6833790482187bda30f4fa067c0b0511
Philip Beeby <lordroke@mac.com>
2018-12-22 16:57:04 +1100

    added information about seats

--- a2/index.php
+++ a2/index.php
@@ -18,7 +18,7 @@
     <header>
         <div>
             <h1 id="top">The Lunardo Cinema<img src="lunardo_logo.png" alt="lunardo logo" height="100" width="100"> </h1>
-            <h3>the magic of the movies</h3>
+            <h3>The true cinema experience</h3>
         </div>
     </header>
 
@@ -34,6 +34,20 @@
     <main>
         <article id='about'>
             <h2>About Us</h2>
+            <p>The Lunardo Cinema was first opened in August 1922, when it was called 'The Portland' and was popular with locals and out-of-towners who would travel from far and wide to experience the magic of the motion picture in one of the finest examples of art deco architecture. At the time of its opening it was one of the only cinemas in Victoria able to show movies in <a href='https://en.wikipedia.org/wiki/70_mm_film' target="_blank">70 mm</a> - a higher resolution film gauge than standard 35mm film. Since that time, we have been a firm favourite with movie-buffs and cinema goers looking to see the magic of movies presented as the film makers intended.
+                <figure>
+                    <!-- Image of portland cinema sourced from https://www.freeimages.com/ all pictures are royalty free-->
+                    <img src="old-portland-cinema.jpg" alt="The Lunardo Cinema" />
+                    <br />
+                    <figcaption>The Lunardo Cinema</figcaption>
+                </figure>
+            </p>
+
+            <p>To ensure we are able to maintain our tradition of presenting films in their best possible light we have recently invested millions of dollars upgrading our projection and sound systems to the lastest <a href='http://www.dolby.com/us/en/platforms/dolby-cinema.html' target="_blank">Dolby Cinema</a> standards. With Dolby Vision projection and Dolby Atmos sound you will see and hear a movie like never before, setting a new standard in cinema immersion.
+            </p>
+            <p>For your viewing comfort we have also refurbished all our seating - we are now proud to offer new levels of upgraded seating in both standard and first-class seating. With our first-class seating we are taking luxury to new heights with our 160-degree motorised reclining leather seats.</p>
+
+            <p>Whether you are local to our town or travelling from further afield you will find visiting the Lunardo is an experience you cannot get at your general multiplex cinema. Please come and experience the true magic of the movies - at The Lunardo Cinema.</p>
             <!-- Creative Commons image sourced from https://pixabay.com/en/maintenance-under-construction-2422173/ and used for educational purposes only -->
             <!--commented out this 'under contstruction image          
         <img src='../../media/website-under-construction.png' alt='Website Under Construction' />-->
@@ -41,6 +55,36 @@
 
         <article id='seats'>
             <h2>Seats and Prices</h2>
+            <p>We now offer two types of seating, Standard and First-Class, both offer much improved levels of comformt and relaxation.
+                <h4>First-Class Seat Features</h4>
+                <ul>
+                    <li>luxurious aesthetic</li>
+                    <li>fully reclining seat</li>
+                    <li>two individual motors</li>
+                    <li>central processor</li>
+                    <li>footrest sensor option</li>
+                    <li>easy lift system</li>
+                    <li>underseat lighting</li>
+                    <li>auto-return footrest option</li>
+                </ul>
+                <figure>
+                    <img src="first-class_Verona-Twin.png" alt="First Class Seat" />
+                    <br />
+                    <figcaption>The new first-class seat with motorised recline</figcaption>
+                </figure>
+
+                <h4>Standard Class Seat Features</h4>
+                <ul>
+                    <li>Leather headrest for extended durability</li>
+                    <li>Multi-angled positioned backrest</li>
+                    <li>No finger traps</li>
+                </ul>
+                <figure>
+                    <img src="standard-seat.jpg" alt="Standard Seat" />
+                    <br />
+                    <figcaption>The new standard seat</figcaption>
+                </figure>
+            </p>
 
         </article>
         <article id='showing'>
@@ -58,7 +102,6 @@
     <footer>
         <div>&copy;<script>
                 document.write(new Date().getFullYear());
-
             </script> Philip Beeby (s3666628), Last modified: 22/12/2018
             <?= date ("Y F d  H:i", filemtime($_SERVER['SCRIPT_FILENAME'])); ?>.</div>
         <div>Disclaimer: This website is not a real website and is being developed as part of a School of Science Web Programming course at RMIT University in Melbourne, Australia.</div>
